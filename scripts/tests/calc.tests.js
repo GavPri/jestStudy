1b046e814b46e59e2074b03b1f20a9bd0ade82de
GavPri <gavinprior5@gmail.com>
2023-06-20 09:33:01 +0000

    initial commit

---
+++ scripts/tests/calc.tests.js
@@ -0,0 +1,18 @@
+const addition = require('../calc');
+
+describe('Calculator', () => {
+    describe('addition', () => {
+        test('Should return 42 for 20 + 22', () =>{
+            expect(additon(20,22)).toBe(42)
+        })
+    })
+    describe('subtraction', () => {
+        
+    })
+    describe('multiplication', () => {
+        
+    })
+    describe('division', () => {
+        
+    })
+})
